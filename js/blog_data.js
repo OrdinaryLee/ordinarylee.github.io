fb590041b4d039438dae488a55260d68e6ffb270
lxc <lixchen1991@qq.com>
2020-01-18 16:15:14 +0800

    updated the js

---
+++ js/blog_data.js
@@ -0,0 +1,16 @@
+var blog_data = [
+    {
+        "title": "常用软件的安装及配置",
+        "path": "/articles//b90baba",
+        "publish_date": "2020-01-18 16:12:27",
+        "update_date": "2020-01-18 16:12:27",
+        "category": ""
+    },
+    {
+        "title": "常用软件的安装及配置",
+        "path": "/articles/Others/9e1b4bc",
+        "publish_date": "2020-01-18 16:15:13",
+        "update_date": "2020-01-18 16:15:13",
+        "category": "Others"
+    }
+]
